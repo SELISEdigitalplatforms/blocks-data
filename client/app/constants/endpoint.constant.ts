c8b580b9528944974643e739a322d452d6e33c1b
noor-selise <noor.mohammad@selisegroup.com>
2026-05-03 17:59:58 +0600

    refactor(endpoint): update UDS API base URL to include versioning
    - Introduced an enum for API versioning.
    - Updated the UDS base URL to reflect the new versioning structure.

--- client/app/constants/endpoint.constant.ts
+++ client/app/constants/endpoint.constant.ts
@@ -1,9 +1,13 @@
 export const IDP_BASE_URL = "https://dev-idp.blocksdevelopers.com";
 
+enum Version {
+  V1 = "v1",
+}
+
 export const API_BASES = {
   COMMUNICATION: "/api",
   CLOUD_CONFIGURATION: "/api",
-  UDS: "/api",
+  UDS: `/api/${Version.V1}`,
   UILM: "/api",
   UTILITIES: "/api",
   CLOUD_BUILD: "/api",

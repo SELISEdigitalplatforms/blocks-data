9cc34f455b6fa9b853ca09f1b70f7256641b0471
noor-selise <noor.mohammad@selisegroup.com>
2026-05-03 18:59:55 +0600

    refactor(endpoint): simplify UDS API base URL
    - Removed the versioning enum and updated the UDS base URL to a static path of "/api" for consistency and clarity.

--- client/app/constants/endpoint.constant.ts
+++ client/app/constants/endpoint.constant.ts
@@ -1,13 +1,9 @@
 export const IDP_BASE_URL = "https://dev-idp.blocksdevelopers.com";
 
-enum Version {
-  V1 = "v1",
-}
-
 export const API_BASES = {
   COMMUNICATION: "/api",
   CLOUD_CONFIGURATION: "/api",
-  UDS: `/uds/${Version.V1}`,
+  UDS: "/api",
   UILM: "/api",
   UTILITIES: "/api",
   CLOUD_BUILD: "/api",

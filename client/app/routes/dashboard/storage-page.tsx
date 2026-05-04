791861ddff440bafc931b9cf66bbe726b356ca69
theshameem75 <shameem.alam@selisegroup.com>
2026-05-04 19:51:04 +0600

    fix: minified react issue

--- client/app/routes/dashboard/storage-page.tsx
+++ client/app/routes/dashboard/storage-page.tsx
@@ -1,10 +1,10 @@
-import { StorageContents } from "@/cross-modules/storage/pages/storage/storage-contents"
+import { StorageContentsWrapper } from "@/cross-modules/storage/pages/storage/storage-contents"
 
 const StoragePage = () => {
   return (
     <main className="flex flex-col gap-6 p-6">
       <h1 className="text-xl font-semibold md:text-2xl">Storage</h1>
-      <StorageContents />
+      <StorageContentsWrapper />
     </main>
   )
 }

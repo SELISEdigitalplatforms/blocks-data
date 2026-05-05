7c0a18c6c66859dddb4894a43573367162b9bc59
theshameem75 <shameem.alam@selisegroup.com>
2026-05-05 15:31:23 +0600

    fix: use utitlities in notification endpoints - FE

--- client/app/cross-modules/communication/notification/constants/endpoint.constant.ts
+++ client/app/cross-modules/communication/notification/constants/endpoint.constant.ts
@@ -1,9 +1,9 @@
 import { API_BASES } from "@/constants/endpoint.constant";
 
 export const NOTIFICATION_ENDPOINTS = {
-  GET_NOTIFICATIONS: `${API_BASES.COMMUNICATION}/Notifier/GetNotifications`,
-  MARK_AS_READ: `${API_BASES.COMMUNICATION}/Notifier/MarkNotificationAsRead`,
-  MARK_ALL_AS_READ: `${API_BASES.COMMUNICATION}/Notifier/MarkAllNotificationAsRead`,
+  GET_NOTIFICATIONS: `${API_BASES.UTILITIES}/Notifier/GetNotifications`,
+  MARK_AS_READ: `${API_BASES.UTILITIES}/Notifier/MarkNotificationAsRead`,
+  MARK_ALL_AS_READ: `${API_BASES.UTILITIES}/Notifier/MarkAllNotificationAsRead`,
 } as const;
 
 export const NOTIFICATION_CONFIG_ENDPOINTS = {

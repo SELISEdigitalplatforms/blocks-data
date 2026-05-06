2749d27d325ea74808e612d23071a46da59cb0de
theshameem75 <shameem.alam@selisegroup.com>
2026-05-06 19:21:52 +0600

    fix: reload api endpoint - FE

--- client/app/cross-modules/data-gateway/services/configuration.service.ts
+++ client/app/cross-modules/data-gateway/services/configuration.service.ts
@@ -62,7 +62,7 @@ class ConfigurationService {
     projectKey: string;
     projectShortKey?: string;
   }): Promise<IDataServiceConfigurationResponse> {
-    const url = `${API_BASES.UDS}/${payload.projectShortKey}/configurations/reload?projectKey=${encodeURIComponent(payload.projectKey)}`;
+    const url = `${API_BASES.UDS}/configurations/reload?projectKey=${encodeURIComponent(payload.projectKey)}`;
     return http.post(url, {});
   }
 

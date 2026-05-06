b5dce9ddb59d7e3c01ef68a766afcaff3e0eaded
noor-selise <noor.mohammad@selisegroup.com>
2026-05-06 09:39:25 +0600

    chore: update .gitignore and modify notification service URL

--- client/app/cross-modules/communication/notification/services/notification.service.ts
+++ client/app/cross-modules/communication/notification/services/notification.service.ts
@@ -14,7 +14,7 @@ export class NotificationService {
     totalNotificationsCount: number;
     notifications: INotification[];
   }> => {
-    const url = `${NOTIFICATION_ENDPOINTS.GET_NOTIFICATIONS}?page=${pageNumber - 1}&pageSize=${pageSize}`;
+    const url = `/utility${NOTIFICATION_ENDPOINTS.GET_NOTIFICATIONS}?page=${pageNumber - 1}&pageSize=${pageSize}`;
     return http.get(url);
   };
 

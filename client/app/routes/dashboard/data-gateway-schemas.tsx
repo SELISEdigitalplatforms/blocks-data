d0d52ad831efeafc7cd34a30bf9619597b1807c9
theshameem75 <shameem.alam@selisegroup.com>
2026-05-10 17:47:05 +0600

    fix: temporary os ping call

--- client/app/routes/dashboard/data-gateway-schemas.tsx
+++ client/app/routes/dashboard/data-gateway-schemas.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react"
 import { DataService } from "@/cross-modules/data-gateway/components/data-service"
 
 const DataGatewaySchemasPage = () => {
+  // TODO: we will remove this later 
+  useEffect(() => {
+    fetch("https://dev-os.blocksdevelopers.com/ping")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <main className="flex flex-col gap-6 p-6">
       <h1 className="text-xl font-semibold md:text-2xl">Data Gateway</h1>

2121feadcbd0653cc3b405f9c9e74b1f083d03f4
theshameem75 <shameem.alam@selisegroup.com>
2026-05-11 18:50:25 +0600

    fix: appsettings configuration setup - BE

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -16,8 +16,6 @@ var cloudBuildSecret = await CloudBuildSecret.ProcessBlocksSecret(VaultType.Azur
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ApplicationConfigurations.ConfigureApiEnv(builder, args);
-
 ApplicationConfigurations.ConfigureServices(builder.Services, GraphQlConstant.GetMessageConfiguration(secret.MessageConnectionString));
 
 builder.Services.Configure<FormOptions>(options =>

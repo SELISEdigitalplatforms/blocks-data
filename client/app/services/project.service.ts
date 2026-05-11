16c5361dddc03deb9cc643cee2e68d30fff639c1
theshameem75 <shameem.alam@selisegroup.com>
2026-05-11 13:54:24 +0600

    fix: idp endpoint in project service

--- client/app/services/project.service.ts
+++ client/app/services/project.service.ts
@@ -1,11 +1,12 @@
 import { http } from "@/lib/http-client";
+import { IDP_BASE_URL } from "@/constants/endpoint.constant";
 import { PROJECT_ENDPOINTS } from "@blocks-identifier/constants/endpoint.constant";
 import { IGetProjectPayload, IGetProjectResponse, IProjectGroup } from "@/models/project.model";
 
 export class ProjectService {
   getProjects(page = 0, pageSize = 100, tenantGroupId = ""): Promise<IProjectGroup[]> {
-    const url = `${PROJECT_ENDPOINTS.GETS}?page=${page}&pageSize=${pageSize}&tenantGroupId=${tenantGroupId}`;
-    return http.get(url);
+    const url = `${IDP_BASE_URL}${PROJECT_ENDPOINTS.GETS}?page=${page}&pageSize=${pageSize}&tenantGroupId=${tenantGroupId}`;
+    return http.get(url, undefined, { absoluteUrl: true });
   }
 
   getProject(payload: IGetProjectPayload): Promise<IGetProjectResponse> {

6c550c4ed94cb494bae4f0cb022801f318c9fdb5
theshameem75 <shameem.alam@selisegroup.com>
2026-05-19 17:51:18 +0600

    fix: optional chaining

--- client/app/pages/dashboard/dashboard-overview.tsx
+++ client/app/pages/dashboard/dashboard-overview.tsx
@@ -11,28 +11,38 @@ import { ActionsListProject } from "@/components/actions-list-project/actions-li
 
 export const DashboardOverview = () => {
   const projectKey = useProjectStore().selectedProject?.tenantId || "";
-  const { itemId } = useProjectStore().selectedProject || { itemId: "", tenantId: "" };
+  const { itemId } = useProjectStore().selectedProject || {
+    itemId: "",
+    tenantId: "",
+  };
   const { data, isLoading } = useGetProject({ projectId: itemId });
   const { mutateAsync } = useValidateCNameProject({ projectKey });
 
   const cNameValidator = useCallback(async () => {
     try {
       if (
-        !data?.data.applicationDomain ||
+        !data?.data?.applicationDomain ||
         getDomain(data.data.applicationDomain) === "seliseblocks.com"
       )
         return;
 
+      if (!data?.data?.customDomain) return;
+
       await mutateAsync({
         projectKey: projectKey,
-        cookieDomain: new URL(data?.data.customDomain).hostname,
+        cookieDomain: new URL(data.data.customDomain).hostname,
       });
     } catch (error) {
       if (error && typeof error === "object" && "errors" in error) {
         showErrorToast({ errors: (error as any).errors });
       }
     }
-  }, [data?.data.applicationDomain, data?.data.customDomain, mutateAsync, projectKey]);
+  }, [
+    data?.data?.applicationDomain,
+    data?.data?.customDomain,
+    mutateAsync,
+    projectKey,
+  ]);
 
   useEffect(() => {
     cNameValidator();
@@ -41,7 +51,9 @@ export const DashboardOverview = () => {
   return (
     <main className="flex flex-col gap-6 p-6">
       <div className="flex items-center justify-between gap-2">
-        <h1 className="text-xl font-semibold md:text-2xl">Environment Overview</h1>
+        <h1 className="text-xl font-semibold md:text-2xl">
+          Environment Overview
+        </h1>
         <ActionsListProject />
       </div>
       <ProjectDetail project={data?.data} isLoading={isLoading} />

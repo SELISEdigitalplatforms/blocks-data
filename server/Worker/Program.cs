06f56bade5d7f5ebb1753c7627220b04805a46d6
theshameem75 <shameem.alam@selisegroup.com>
2026-05-21 08:08:58 +0600

    fix: configure worker env - BE

--- server/Worker/Program.cs
+++ server/Worker/Program.cs
@@ -23,7 +23,7 @@ IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
         .ConfigureAppConfiguration((context, builder) =>
         {
-            // ApplicationConfigurations.ConfigureWorkerEnv(builder, args);
+            ApplicationConfigurations.ConfigureWorkerEnv(builder, args);
         })
         .ConfigureServices((services) =>
         {

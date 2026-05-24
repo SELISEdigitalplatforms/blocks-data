d469252f60f51938d19bbc7ddd0c5b1a16ca0824
theshameem75 <shameem.alam@selisegroup.com>
2026-05-24 12:05:21 +0600

    fix: tenant id property for impersonate

--- client/app/services/impersonation.service.ts
+++ client/app/services/impersonation.service.ts
@@ -1,24 +1,23 @@
-
-import { IMPERSONATE_ENDPOINTS } from '@/idp/authentication/constants'
-import { http } from '@/lib/http-client'
+import { IMPERSONATE_ENDPOINTS } from "@/idp/authentication/constants";
+import { http } from "@/lib/http-client";
 
 export interface ImpersonationRequest {
-  targetTenantId: string
-  orgId?: string
-  organizationId?: string
+  targeted_tenant_id: string;
+  orgId?: string;
+  organizationId?: string;
 }
 
 export interface ImpersonationState {
-  rootTenantId: string
-  targetTenantId: string
-  orgId: string
-  startedAtUtc: string
+  rootTenantId: string;
+  targeted_tenant_id: string;
+  orgId: string;
+  startedAtUtc: string;
 }
 
 export interface ImpersonationStatusResponse {
-  impersonated: boolean
-  originalTenantId: string
-  impersonatedTenantId: string | null
+  impersonated: boolean;
+  originalTenantId: string;
+  impersonatedTenantId: string | null;
 }
 
 class ImpersonationService {
@@ -30,16 +29,16 @@ class ImpersonationService {
       request,
       undefined,
       { absoluteUrl: true },
-    )
+    );
   }
 
   stopImpersonation(): Promise<void> {
     return http.post(
       `${IMPERSONATE_ENDPOINTS.STOP_IMPERSONATION}`,
-      null,
+      {},
       undefined,
       { absoluteUrl: true },
-    )
+    );
   }
 
   impersonationStatus(): Promise<ImpersonationStatusResponse> {
@@ -48,8 +47,8 @@ class ImpersonationService {
       null,
       undefined,
       { absoluteUrl: true },
-    )
+    );
   }
 }
 
-export const impersonationService = new ImpersonationService()
+export const impersonationService = new ImpersonationService();

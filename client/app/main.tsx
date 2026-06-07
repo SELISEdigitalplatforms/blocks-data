033b3e77606e4b32b39c5920017ea85d169d2588
theshameem75 <shameem.alam@selisegroup.com>
2026-06-07 16:38:42 +0600

    fix: package for blocks

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -7,13 +7,21 @@ import { TooltipProvider } from "./components/ui-kits/tooltip/tooltip";
 import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
 import "./styles/globals.css";
+import { BlocksAppLayout } from "@seliseblocks/blocks-kit";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
       <NuqsAdapter>
         <TooltipProvider>
-          <RouterProvider router={router} />
+          <BlocksAppLayout
+            config={{
+              userBaseUrlKey: "BLOCKS_IAM_BASE_URL",
+              projectBaseUrlKey: "BLOCKS_DATA_BASE_URL",
+            }}
+          >
+            <RouterProvider router={router} />{" "}
+          </BlocksAppLayout>
           <Toaster />
         </TooltipProvider>
       </NuqsAdapter>

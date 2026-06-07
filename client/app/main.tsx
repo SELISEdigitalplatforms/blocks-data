a12f922dd675eaa494074081eddffb8d6007b34f
theshameem75 <shameem.alam@selisegroup.com>
2026-06-08 02:39:04 +0600

    fix: config values from runtime env

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -8,6 +8,7 @@ import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
 import "./styles/globals.css";
 import { BlocksAppLayout } from "@seliseblocks/blocks-kit";
+import { getRuntimeEnv } from "./lib/runtime-env";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,8 +17,8 @@ createRoot(document.getElementById("root")!).render(
         <TooltipProvider>
           <BlocksAppLayout
             config={{
-              userBaseUrlKey: "BLOCKS_IAM_BASE_URL",
-              projectBaseUrlKey: "BLOCKS_LOGIC_BASE_URL",
+              userBaseUrlKey: getRuntimeEnv("BLOCKS_IAM_BASE_URL"),
+              projectBaseUrlKey: getRuntimeEnv("BLOCKS_LOGIC_BASE_URL"),
               appLogoUrl: {
                 dark: "/blocks-logos/Logo_Black.svg",
                 light: "/blocks-logos/Logo_White.svg",

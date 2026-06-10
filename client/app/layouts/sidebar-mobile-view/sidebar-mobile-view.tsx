9f21768158635bb47dfd2def300d5fc6396fc695
theshameem75 <shameem.alam@selisegroup.com>
2026-06-10 13:08:46 +0600

    feat: overview and workspace in dashboard

--- client/app/layouts/sidebar-mobile-view/sidebar-mobile-view.tsx
+++ client/app/layouts/sidebar-mobile-view/sidebar-mobile-view.tsx
@@ -38,7 +38,7 @@ export function SidebarMobileView() {
       >
         <SheetHeader className="h-[60px] px-4 py-3">
           <SheetTitle className="flex items-center justify-between">
-            <Link to="/services/authentication">
+            <Link to="/console">
               <Logo width={72} height={36} className="h-9 w-auto" />
             </Link>
             <SheetClose className="!mt-0">
@@ -63,7 +63,11 @@ export function SidebarMobileView() {
         <nav className="grid gap-2">
           {allowedMenu.map((menu) => (
             <Fragment key={menu.id}>
-              <MobileMenuItem menu={menu} onClick={() => setOpen(false)} />
+              {menu.type === "menu" ? (
+                <MobileMenuItem menu={menu} onClick={() => setOpen(false)} />
+              ) : (
+                <Separator />
+              )}
             </Fragment>
           ))}
         </nav>

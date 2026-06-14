d9c5ca637c55740c40f6494fc53cae23516ccb7c
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-06-14 21:01:22 +0600

    add ConfigureApiEnv

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -16,6 +16,8 @@ var cloudBuildSecret = await CloudBuildSecret.ProcessBlocksSecret(VaultType.Azur
 
 var builder = WebApplication.CreateBuilder(args);
 
+ApplicationConfigurations.ConfigureApiEnv(builder, args);
+
 ApplicationConfigurations.ConfigureServices(builder.Services, GraphQlConstant.GetMessageConfiguration(secret.MessageConnectionString));
 
 builder.Services.Configure<FormOptions>(options =>

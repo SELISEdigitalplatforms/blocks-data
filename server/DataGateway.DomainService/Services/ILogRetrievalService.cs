dc7608ed664a777dab58ded66de75c0a9325ad37
theshameem75 <shameem.alam@selisegroup.com>
2026-06-23 17:20:13 +0600

    feat: removed cloud secrets and pod builds business

--- server/DataGateway.DomainService/Services/ILogRetrievalService.cs
+++
@@ -1,6 +0,0 @@
-namespace DataGateway.DomainService.Services;
-
-public interface ILogRetrievalService
-{
-    Task CheckDataGatewayLog(string pipelineRunName, string tenantId, int? pollingIntervalMs = null, int? maxPollingDurationMs = null);
-}

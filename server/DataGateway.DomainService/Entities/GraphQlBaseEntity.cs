6563e8fd127c33eb505b24778b2911b0fb7ad0e9
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-06-23 11:06:37 +0600

    change OrganizationIds to OrganizationId in GraphQlBaseEntity and related files

--- server/DataGateway.DomainService/Entities/GraphQlBaseEntity.cs
+++ server/DataGateway.DomainService/Entities/GraphQlBaseEntity.cs
@@ -7,6 +7,6 @@ public class GraphQlBaseEntity : BaseEntity
 {
     public DateTime? DeletedDate { get; set; }
     public bool IsDeleted { get; set; }
-    public List<string> OrganizationIds { get; set; } = new List<string>();
+    // public List<string> OrganizationIds { get; set; } = new List<string>();
 
 }

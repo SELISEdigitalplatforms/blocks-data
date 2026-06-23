ffc7c071ca4cd2f798960bc53c5982da3265eec5
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-06-23 14:53:45 +0600

    fix OrganizationId property and related references

--- server/DataGateway.DomainService/Entities/GraphQlBaseEntity.cs
+++ server/DataGateway.DomainService/Entities/GraphQlBaseEntity.cs
@@ -7,6 +7,5 @@ public class GraphQlBaseEntity : BaseEntity
 {
     public DateTime? DeletedDate { get; set; }
     public bool IsDeleted { get; set; }
-    // public List<string> OrganizationIds { get; set; } = new List<string>();
 
 }

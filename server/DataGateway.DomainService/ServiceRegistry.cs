8c45879f90c6ce7e806d0a4ae3f0d101adca8365
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-06-24 15:53:26 +0600

    make singleton

--- server/DataGateway.DomainService/ServiceRegistry.cs
+++ server/DataGateway.DomainService/ServiceRegistry.cs
@@ -71,7 +71,7 @@ public static class ServiceRegistry
     }
     public static void RegisterGraphQlServices(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddScoped<IConfigurationService, ConfigurationService>();
+        serviceCollection.AddSingleton<IConfigurationService, ConfigurationService>();
         serviceCollection.AddSingleton<IGqlDbRepository, GqlDbRepository>();
         serviceCollection.AddSingleton<GraphqlSchemaBuilder>();
         serviceCollection.AddSingleton<IDataChangeEventPublisher, DataChangeEventPublisher>();

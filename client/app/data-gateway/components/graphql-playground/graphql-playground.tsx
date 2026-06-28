1cf7f333b0b68edb49b37f283b0d89d62c4051d2
theshameem75 <shameem.alam@selisegroup.com>
2026-06-28 20:17:11 +0600

    feat: latest blocks kit package version

--- client/app/data-gateway/components/graphql-playground/graphql-playground.tsx
+++ client/app/data-gateway/components/graphql-playground/graphql-playground.tsx
@@ -8,7 +8,7 @@ export const GraphQLPlayground = () => {
     <main className="flex h-full min-h-0 w-full flex-col gap-4 p-6">
       <div className="flex w-full flex-col gap-2">
         <div className="hidden md:flex">
-          <PageBreadcrumb breadcrumbIndex={2} />
+          <PageBreadcrumb breadcrumbIndex={3} />
         </div>
       </div>
       <div className="w-full flex-1 overflow-hidden rounded-md border bg-background">

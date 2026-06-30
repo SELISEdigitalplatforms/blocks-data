59f9f89dad2310a7716243629b7163613919fe28
theshameem75 <shameem.alam@selisegroup.com>
2026-06-30 13:06:06 +0600

    feat: project key added in query client

--- client/app/storage/hooks/use-storage-configuration.ts
+++ client/app/storage/hooks/use-storage-configuration.ts
@@ -2,35 +2,39 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { storageService } from "../services/storage.service";
 import { useProjectStore } from "@seliseblocks/blocks-kit";
 
+const getProjectKey = () => useProjectStore.getState().selectedProject?.tenantId || "";
+
 export const useGetStorageConfigurations = () => {
-  const tenantId = useProjectStore().selectedProject?.tenantId || "";
+  const projectKey = getProjectKey();
   return useQuery({
-    queryKey: ["storage", "configuration", "gets", tenantId],
-    queryFn: () => storageService.configuration.gets(tenantId),
+    queryKey: ["storage", "configuration", "gets", projectKey],
+    queryFn: () => storageService.configuration.gets(projectKey),
   });
 };
 
 export const useSaveStorageConfiguration = () => {
   const queryClient = useQueryClient();
+  const projectKey = getProjectKey();
   return useMutation({
     mutationKey: ["storage", "configuration", "save"],
     mutationFn: storageService.configuration.save,
     onSuccess: (data) => {
       if (data.isSuccess)
         queryClient.invalidateQueries({
-          queryKey: ["storage", "configuration", "gets"],
+          queryKey: ["storage", "configuration", "gets", projectKey],
         });
     },
   });
 };
 export const useDeleteStorageConfiguration = () => {
   const queryClient = useQueryClient();
+  const projectKey = getProjectKey();
   return useMutation({
     mutationKey: ["storage", "configuration", "delete"],
     mutationFn: storageService.configuration.delete,
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["storage", "configuration", "gets"],
+        queryKey: ["storage", "configuration", "gets", projectKey],
       });
     },
   });

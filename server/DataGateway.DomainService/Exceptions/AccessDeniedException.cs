fe39d3b7a4d97a831b99a0dfe2fcc05a067339d4
theshameem75 <shameem.alam@selisegroup.com>
2026-07-15 16:04:39 +0600

    refactor(api): remove REST gateway and transition to GraphQL-only

--- server/DataGateway.DomainService/Exceptions/AccessDeniedException.cs
+++
@@ -1,9 +0,0 @@
-namespace DataGateway.DomainService.Exceptions;
-
-public class AccessDeniedException : DataGatewayException
-{
-    public AccessDeniedException(string message, string errorCode = "AUTH_NOT_AUTHENTICATED")
-        : base(message, errorCode, 401)
-    {
-    }
-}
